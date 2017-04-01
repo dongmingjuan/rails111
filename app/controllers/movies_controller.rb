8d253c9e48b7678e5c1195358a3f3a53b19b0dff
dongmingjuan <d387918156@gmail.com>
2017-04-01 14:34:31 +0800

    implement movies#new

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,14 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
-end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to movies_path
+  end
+  end
