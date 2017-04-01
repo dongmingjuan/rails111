f6f0f19cef15570eb67a317d9231bc7889eb7b61
dongmingjuan <d387918156@gmail.com>
2017-04-01 14:51:22 +0800

    implement groups edit&show&destroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,10 +7,30 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
   def create
     @movie = Movie.new(movie_params)
     @movie.save
     redirect_to movies_path
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to movies_path, notice: "Update success!"
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, alert: "Movie deleted!"
   end
 
   private
